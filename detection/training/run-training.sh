311d0f5a6bf1b0e327873527b4e6782652a08596
Hafizur Rahman <bibagimon@gmail.com>
2019-04-21 11:56:52 +0900

    Updated faster_rnn_resnet101.config

--- detection/training/run-training.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export PYTHONPATH=$PYTHONPATH:/tensorflow/models:/tensorflow/models/slim
-
-python3 object_detection/train.py --logtostderr \
-    --train_dir=./models/train \
-    --pipeline_config_path=/training/faster_rnn_resnet101.config
